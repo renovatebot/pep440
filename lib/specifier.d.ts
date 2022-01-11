8af8cdf552369bb34f39264fadbd565b1e4212d6
RahulGautamSingh <rahultesnik@gmail.com>
2022-01-11 19:13:21 +0545

    remove comment

--- lib/specifier.d.ts
+++ lib/specifier.d.ts
@@ -2,29 +2,6 @@ import type { Pep440Version, Pep440Constraint } from "./shared";
 
 export const RANGE_PATTERN: string;
 
-/*
-   parse function takes a string and returns an object that has the values of both th ebelow interfaces combined
-  interface Pep440Constraint {
-    operator: string;
-    prefix: string;
-    version: string;
-  }
-  * interface Pep440Version{
-  public: string;
-  base_version: string;
-  is_prerelease: boolean;
-  is_devrelease: boolean;
-  is_postrelease: boolean;
-  epoch: number;
-  release: number[];
-  pre: (string | number)[];
-  post: (string | number)[];
-  dev: (string | number)[];
-  local: string | null;
-}
- still I have used interface Pep440Constraint as its return type cause it is being used in the file
- lib/versioning/pep440/range.ts as so L52
-  */
 export function filter(versions: string[], range: string): string[];
 export function maxSatisfying(
   version: string,
