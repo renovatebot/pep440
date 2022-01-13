1daddb6c3ead298727cbc4445967c5eb8805c25c
RahulGautamSingh <rahul_ug@cse.nits.ac.in>
2022-01-13 20:20:33 +0545

    Update lib/specifier.d.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/specifier.d.ts
+++ lib/specifier.d.ts
@@ -18,5 +18,9 @@ export function minSatisfying(
   options: Pep440SpecifierOptions
 ): string | null;
 export function parse(ranges: string): Pep440Constraint[]; // have doubts regarding this which need to be discussed
-export function satisfies(version: string, specifier: string): boolean;
+export function satisfies(
+  version: string,
+  specifier: string,
+  options: Pep440SpecifierOptions
+): boolean;
 export function validRange(specifier: string): boolean;
